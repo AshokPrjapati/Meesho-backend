911b7773fa79432df4b7c9a5dc7386f75b2ea53b
Ashok Kumar <ap271449@gmail.com>
2023-03-04 22:48:08 +0530

    backend updated

--- Controllers/cart.controller.js
+++ Controllers/cart.controller.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
-const bcrypt = require("bcrypt");
-const { CartModel } = require("../Models/user.model")
+const { CartModel, AddressModel } = require("../Models/user.model")
 
 
 // adding to cart
 async function AddToCart(req, res) {
-    const payload = req.body;
+    let id = req.body.productID;
     try {
-        let cartProduct = new CartModel(payload);
+        let isExists = await CartModel.findOne({ productID: id });
+        if (isExists) return res.status(201).send({ status: 201, "message": "Product already exists in cart" });
+        let cartProduct = new CartModel(req.body);
         await cartProduct.save();
-        res.status(200).send({ "message": "Product added successfully in cart" });
+        res.status(200).send({ "message": "Product added successfully in cart", cartProduct });
     } catch (error) {
         console.log(error);
         res.status(400).send({ message: error.message });
@@ -18,10 +19,11 @@ async function AddToCart(req, res) {
 
 // remove to cart
 async function RemoveFromCart(req, res) {
-    const { id } = req.body;
+    const { id, user } = req.body;
     try {
         await CartModel.findByIdAndRemove({ _id: id });
-        res.status(200).send({ message: " cart product is removed" });
+        const cartProducts = await CartModel.find({ user });
+        res.status(200).send({ message: "cart product is removed", cartProducts });
     } catch (error) {
         console.log(error);
         res.status(400).send({ message: error.message });
@@ -49,7 +51,7 @@ async function CartTotal(req, res) {
         cartProducts.forEach((p) => {
             total += p.price * p.quantity;
         });
-        res.status(200).send({ total });
+        res.status(200).send({ message: "total is updated", total });
 
     } catch (error) {
         console.log(error);
@@ -72,25 +74,32 @@ async function UpdateCart(req, res) {
     }
 }
 
-
-// order suceess
-
-async function PlaceOrder(req, res) {
-    const { _id, ...payload } = req.body;
+// adding addresss to db
+async function AddAddress(req, res) {
+    const payload = req.body;
     try {
-        let cart = await CartModel.findOne({ _id });
-        Object.assign(cart, payload);
-        await cart.save();
-        return res.status(201).json({ message: 'cart quantity has been updated' })
+        let address = new AddressModel(payload);
+        await address.save();
+        res.status(200).send({ "message": "address added successfully in cart" });
     } catch (error) {
         console.log(error);
-        return res.status(201).json({ error: error.message })
+        res.status(400).send({ message: error.message });
     }
 }
 
+// getting address dfrom db
+async function GetAddress(req, res) {
+    let { user } = req.body;
+    try {
+        let address = await AddressModel.find({ user });
+        res.status(200).send({ message: "cart products fetched successfully", address });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({ message: error.message });
+    }
+}
 
 // on order placing
-
 async function placeOrder(req, res) {
     const user = req.body.user;
     try {
@@ -104,7 +113,7 @@ async function placeOrder(req, res) {
 
         // remove cart items
         await CartModel.deleteMany({ user });
-        res.status(200).send({ message: 'Order placed successfully' });
+        res.status(200).send({ message: 'Order placed successfully', orderItems: cartItems });
 
     } catch (error) {
         console.log(error);
@@ -119,5 +128,7 @@ module.exports = {
     GetCart,
     CartTotal,
     UpdateCart,
-    placeOrder
+    placeOrder,
+    AddAddress,
+    GetAddress
 }
